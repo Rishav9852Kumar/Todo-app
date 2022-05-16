f83497d203873bf30abb5f031aff52fe495abf02
Rishav_Kumar <rishavkumaraug20005212@gmail.com>
2022-05-16 19:56:22 +0530

    new

---
+++ src/Components/todoForm.js
@@ -0,0 +1,60 @@
+import React ,{useStates,useContext} from "react";
+import{
+   FormGroup,
+   Input,
+   Button,
+   Form,
+   InputGroup,
+   CardGroup
+} from "reactstrap";
+
+import {v4} from "uuid"
+import {TodoContext} from "../context/TodoContext"
+import {ADD_TODO} from "../context/action.types"
+
+const TodoForm = () =>{
+    //Defining a State where we staore the values//
+    const [todoString,setTodoString] = useStates("");
+    const {dispatch} = useContext(TodoContext);
+
+
+    const handleSubmit = e=>{
+        e.preventDefault();
+        if(todoString === "")
+        {
+            return alert("Please Enter a Todo");
+        }
+        const todo ={
+            todoString,
+            id: v4()
+        }
+        dispatch({
+            type: ADD_TODO,
+            payload: todo
+        })
+        setTodoString("");
+    }
+    return(
+        <Form onSubmit ={handleSubmit}>
+            <FormGroup>
+                <InputGroup>
+                <Input 
+                type="text"
+                name="todo"
+                id="todo"
+                placeholder="Your next To DO "
+                //Initialising the variable value to the todoString//
+                value={todoString}
+                onChange={e => setTodoString(e.target.value)}
+                />
+                <CardGroup >
+                    <Button color="warning">Add</Button>
+               </CardGroup>
+                </InputGroup>
+            </FormGroup>
+
+        </Form>
+
+    );
+}
+export default TodoForm;
---
+++ src/context/action.types.js
@@ -0,0 +1,3 @@
+export const ADD_TODO ="ASD_TODO";
+export const REMOVE_TODO ="REMOVE_TODO";
+
